27e90d37f5efb1beed2038c8a222eaef5eec3651
Ernesto Stifano <ernesto.stifano@codistica.com>
2023-01-24 17:59:52 +0100

    Add case

--- babel-plugin-custom.js
+++ babel-plugin-custom.js
@@ -7,6 +7,12 @@ const single = 'line1';
 
 module.exports = function babelPluginCustom() {
     const isSingle = process.env.NPM_CONFIG_SINGLE === 'true';
+    const skip = process.env.NPM_CONFIG_SKIP === 'true';
+
+    if (skip) {
+        return {};
+    }
+
     return {
         visitor: {
             Program(path) {
